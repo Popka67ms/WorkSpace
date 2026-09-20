52a11f77c1e1e0999539a157e307b925ada5daf5
Popka67ms <dimav1212@gmail.com>
2026-09-20 21:16:33 +0400

    Implement reverseDegree function in Solution class

--- Test1/main.cpp
+++ Test1/main.cpp
@@ -1,1 +1,17 @@
 
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int reverseDegree(string s) 
+    {
+        long long total = 0;
+        for (int i = 0; i < s.length(); i++) 
+        {
+            int rev_pos = 26 - (s[i] - 'a');
+            total +=rev_pos * (i + 1);
+        }
+        return total;
+    }
+};

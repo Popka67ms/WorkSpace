96f3f7070ce566484537e6b0689f249c2f1b7995
Popka67ms <dimav1212@gmail.com>
2026-09-20 21:32:27 +0400

    Update main.cpp

--- Test3.Length of Last Word/main.cpp
+++ Test3.Length of Last Word/main.cpp
@@ -1,1 +1,24 @@
+#include <string>
+using namespace std;
+class Solution {
+public:
+    int lengthOfLastWord(string s)
+     {
+    
+        while (!s.empty() && s.back() == ' ') 
+        {
+            s.pop_back();
+        }
+        int last_space_index = -1; 
 
+        for (int i = 0; i < s.length(); i++) 
+        {
+            if (s[i] == ' ') {
+                last_space_index = i; 
+            }
+        }
+        int start_pos = last_space_index + 1;
+        string last_word = s.substr(start_pos);
+        return last_word.length();
+    }
+};
